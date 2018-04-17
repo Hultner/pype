7e8ebd5eeffc0e7da6d027277bda3ca094bb065f
Alexander Hultnér <ahultner@gmail.com>
2018-04-17 18:02:12 +0200

    Added some documentation around the project

--- pype.py
+++ pype.py
@@ -1,3 +1,23 @@
+"""Pype, python pipelines
+
+Author: Alexander Hultnér
+
+The idea is to allow for pipestyle programming in python eg.
+p(
+  [1,2,3]
+  | lambda x: x*2
+  | sum
+)
+> 12
+
+Roadmap:
+    [x] Basic PoC using commas instead of pipes
+    [ ] Use introspection to allow usage with the "|" pipe character
+    [ ] Add generative testing using hypothesis
+    [ ] Set up static analysis and automatic testing (travis and codeclimate)
+
+"""
+
 def pipe(in_, *args: "Function"):
     """Basic pipe functionality
 
