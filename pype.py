b5d5780c521d8dff11d5316eb4421ee9f8b8fa4b
Alexander Hultnér <ahultner@gmail.com>
2018-04-17 18:03:16 +0200

    Further expanded roadmap

--- pype.py
+++ pype.py
@@ -13,8 +13,11 @@ p(
 Roadmap:
     [x] Basic PoC using commas instead of pipes
     [ ] Use introspection to allow usage with the "|" pipe character
+    [ ] Set up proper python module with setup.py
     [ ] Add generative testing using hypothesis
     [ ] Set up static analysis and automatic testing (travis and codeclimate)
+    [ ] Publish to pypi
+    [ ] Write a short article on the project and how it can be used
 
 """
 
